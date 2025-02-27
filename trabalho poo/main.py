f24490e01cd16326b956859b128e15c1b62fad1e
Lulu042014 <luizamelonogueira@alu.ufc.br>
2025-02-27 15:39:21 -0300

    Update main.py

--- trabalho poo/main.py
+++ trabalho poo/main.py
@@ -1,5 +1,5 @@
-from moto_esportiva import MotoEsportiva
-from moto_corrida import MotoCorrida
+from moto_esportiva import STREETBOB
+from moto_corrida import Kawasaki_Ninja_400
 
 def test_drive(moto):
     print(f'\nTestando {moto.__class__.__name__}')
@@ -7,13 +7,13 @@ def test_drive(moto):
     moto.exibe_velocidade()
 
 if __name__ == '__main__':
-    moto_esportiva = MotoEsportiva(100)
-    print('Moto Esportiva:')
+    moto_esportiva = STREET_BOB(100)
+    print('STREET BOB:')
     moto_esportiva.turbo()
     moto_esportiva.exibe_velocidade()
     moto_esportiva.freia()
     moto_esportiva.exibe_velocidade()
     test_drive(moto_esportiva)
 
-    moto_corrida = MotoCorrida(50)
+    moto_corrida = Kawasaki_Ninja_400(50)
     test_drive(moto_corrida)
