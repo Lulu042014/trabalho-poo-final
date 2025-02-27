07e3fe75c17efeeadf20f647a6f64e7b63772470
Lulu042014 <luizamelonogueira@alu.ufc.br>
2025-02-27 15:59:19 -0300

    Update main.py

--- trabalho poo/main.py
+++ trabalho poo/main.py
@@ -1,5 +1,5 @@
-from moto_esportiva import Harley_Davidson_Street_750
-from moto_corrida import Kawasaki_Ninja_400
+from moto_usual import Harley_Davidson_Street_750
+from moto_esportiva import Kawasaki_Ninja_400
 
 def test_drive(moto):
     print(f'\nTestando {moto.__class__.__name__}')
@@ -7,13 +7,13 @@ def test_drive(moto):
     moto.exibe_velocidade()
 
 if __name__ == '__main__':
-    moto_esportiva = Harley_Davidson_Street_750(100)
+    moto_usual = Harley_Davidson_Street_750(100)
     print('Harley_Davidson_Street_750:')
-    moto_esportiva.turbo()
-    moto_esportiva.exibe_velocidade()
-    moto_esportiva.freia()
-    moto_esportiva.exibe_velocidade()
+    moto_usual.turbo()
+    moto_usual.exibe_velocidade()
+    moto_usual.freia()
+    moto_usual.exibe_velocidade()
     test_drive(moto_esportiva)
 
-    moto_corrida = Kawasaki_Ninja_400(50)
+    moto_esportiva = Kawasaki_Ninja_400(50)
     test_drive(moto_corrida)
